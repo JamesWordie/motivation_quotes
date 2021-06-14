3b1f98b1693889cf408c75e5159afdfe59bc85e6
James Wordie <james.wordie@yahoo.com>
2021-06-14 11:03:05 +0100

    added seeds to the database

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,6 @@ class Quote < ApplicationRecord
   has_many :user_quotes
   has_many :users, through: :user_quotes
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
   validates :author, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Quote.destroy_all
+puts 'Cleaning the database.'
+
+url = "https://zenquotes.io/api/quotes"
+parsed = JSON.parse(URI.open(url).read)
+parsed.each do |quote|
+  Quote.create!(
+    title: quote['q'],
+    author: quote['a']
+    )
+end
+
+puts "Seeded the database"
