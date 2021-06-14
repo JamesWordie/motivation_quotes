e53965c9a890ae3df7d4e1e7011b373d32de64e6
James Wordie <james.wordie@yahoo.com>
2021-06-14 10:52:28 +0100

    added validations to the models

---
+++ app/models/quote.rb
@@ -0,0 +1,7 @@
+class Quote < ApplicationRecord
+  has_many :user_quotes
+  has_many :users, through: :user_quotes
+
+  validates :title, presence: true, uniqueness: true
+  validates :author, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_092658) do
+ActiveRecord::Schema.define(version: 2021_06_14_094439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "quotes", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_quotes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "quote_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["quote_id"], name: "index_user_quotes_on_quote_id"
+    t.index ["user_id"], name: "index_user_quotes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +39,12 @@ ActiveRecord::Schema.define(version: 2021_06_14_092658) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_quotes", "quotes"
+  add_foreign_key "user_quotes", "users"
 end
