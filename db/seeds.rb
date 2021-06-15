f29bcd176a1a44ae7ada9d8f2bbb905af2a83d54
James Wordie <james.wordie@yahoo.com>
2021-06-15 09:33:43 +0100

    created a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,16 @@ require 'json'
 Quote.destroy_all
 puts 'Cleaning the database.'
 
+puts 'Admin user'
+admin = User.create!(email: 'admin@quotes.com', firstname: 'admin', lastname: 'user', password: 123456)
+
 url = "https://zenquotes.io/api/quotes"
 parsed = JSON.parse(URI.open(url).read)
 parsed.each do |quote|
   Quote.create!(
     title: quote['q'],
-    author: quote['a']
+    author: quote['a'],
+    user_id: admin.id
     )
 end
 
