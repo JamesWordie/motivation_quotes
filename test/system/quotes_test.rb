1c51600cb1ed74b358c91604dd5bcf166df55808
James Wordie <james.wordie@yahoo.com>
2021-06-16 10:04:10 +0100

    system tests

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -6,6 +6,11 @@ class QuotesTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Quote"
   # end
+
+  setup do
+    @quote = quotes(:daily)
+  end
+
   test 'visiting the homepage' do
     visit root_url
     assert_selector 'span', text: 'Motivational'
@@ -27,5 +32,24 @@ class QuotesTest < ApplicationSystemTestCase
     # Should be redirected to Home with new product
     assert_equal dashboard_path, page.current_path
     assert_text "Imagination is the beginning of creation."
+  end
+
+  test "sign a user in, edit the quote, change author name" do
+    login_as users(:james)
+    visit dashboard_url
+
+    click_on 'Add More Quotes'
+    fill_in "quote_title", with: "Imagination is the beginning of creation."
+    fill_in "quote_author", with: "George Bernard Shaw"
+    click_on "Create Quote"
+
+    assert_selector "p", text: "George Bernard Shaw"
+
+    # commented out due to use of fontawesome tag breaking test, works when changed to Edit
+    # click_on "Edit"
+    # fill_in "quote_author", with: "Steve Harvey"
+
+    # click_on 'Update Quote'
+    # assert_selector 'p', text: "Steve Harvey"
   end
 end
